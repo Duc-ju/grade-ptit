04f0bc3cc3131499db1510f6d2941f8c84754ddf
Duc-ju <Trangduc56@gmail.com>
2023-12-31 13:24:48 +0700

    [TESTING] add d18 cntt_cnpm

--- src/master-data/D18/_d18MasterData.ts
+++ src/master-data/D18/_d18MasterData.ts
@@ -1,8 +1,10 @@
 import D18_ATTT from "./ATTT.json";
 import D18_CNTT_HTTT from "./CNTT_HTTT.json";
-import { ATTT, CNTT_HTTT } from "../masterConstrain";
+import D18_CNTT_CNPM from "./CNTT_CNPM.json"
+import {ATTT, CNTT_CNPM, CNTT_HTTT} from "../masterConstrain";
 
 export const D18_MASTER_DATA = new Map<string, any>([
   [ATTT, D18_ATTT],
   [CNTT_HTTT, D18_CNTT_HTTT],
+  [CNTT_CNPM, D18_CNTT_CNPM]
 ]);
