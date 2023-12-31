dbc4b5498c2a837f97851309d1ee58b0a60ff302
duc-nguyen-trang <Trangduc56@gmail.com>
2023-12-31 14:07:25 +0700

    [TESTING] add d18 kt

--- src/master-data/D18/_d18MasterData.ts
+++ src/master-data/D18/_d18MasterData.ts
@@ -1,8 +1,10 @@
 import D18_ATTT from "./ATTT.json";
 import D18_CNTT_HTTT from "./CNTT_HTTT.json";
-import { ATTT, CNTT_HTTT } from "../masterConstrain";
+import D18_KT from "./KT.json";
+import { ATTT, CNTT_HTTT, KT } from "../masterConstrain";
 
 export const D18_MASTER_DATA = new Map<string, any>([
   [ATTT, D18_ATTT],
   [CNTT_HTTT, D18_CNTT_HTTT],
+  [KT, D18_KT],
 ]);
